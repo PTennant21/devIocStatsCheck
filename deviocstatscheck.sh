dc3f33c5a8092eaa5b7ccec6ad7429c4fe46ef15
PTennant21 <Pattennant2005@gmail.com>
2024-11-21 11:14:42 -0500

    Update deviocstatscheck.sh
    Better optimization with check function used.

--- deviocstatscheck.sh
+++ deviocstatscheck.sh
@@ -1,119 +1,63 @@
 #!/usr/bin/bash
 
-echo -e "Type your IOC directory. \c"
-read EPICS_IOCS
-#EPICS_IOCS="/epics/iocs"
-
-echo -e "Type the name of your IOC. \c "
-read IOC
-#IOC="istats"
-
+# Checks if argument 1 is found in argument 2. Argument 3 set to true if argument 2 has been searched previously.
+check ()
+{
+	local condition=$1
+	local path=$2
+	local notrepeat=$3
+	
+	case `grep -F $condition $path &> /dev/null; echo $?` in
+		0)
+			echo "$condition found in $path"
+			((errors--))
+			;;
+		1)
+			echo "[WARNING] $condition not found in $path"
+			;;
+		*)
+			echo "[WARNING] $path not found"
+			if $notrepeat ; then
+				((missing++))
+			fi
+			;;
+	esac
+}
 errors=7
 missing=0
 
-echo "Searching the in '$EPICS_IOCS/$IOC'"
+echo "Beginning search in '$IOCBASE/$IOC'"
 
-if [ -d "$EPICS_IOCS/$IOC" ]; then
+if [ -d "$IOCBASE/$IOC" ] && [ "$IOC" != "" ] && [ "$IOCBASE" != "" ]; then
 
 	# Checking for IOCADMIN set in configure/RELEASE
-	case `grep -F "IOCADMIN=" "$EPICS_IOCS/$IOC/configure/RELEASE" &> /dev/null; echo $?` in
-		0)
-			echo "IOCADMIN set in $EPICS_IOCS/$IOC/configure/RELEASE"
-			((errors--))
-			;;
-		1)
-			echo "[WARNING] IOCADMIN not set in $EPICS_IOCS/$IOC/configure/RELEASE"
-			;;
-		*)
-			echo "[WARNING] $EPICS_IOCS/$IOC/configure/RELEASE not found"
-			((missing++))
-			;;
-	esac
+	check "IOCADMIN" "$IOCBASE/$IOC/configure/RELEASE" true
 
 	# Checking for IOCADMIN set in envPaths
-	case `grep -F "epicsEnvSet(\"IOCADMIN\"" "$EPICS_IOCS/$IOC/iocBoot/ioc$IOC/envPaths" &> /dev/null; echo $?` in
-		0)
-			echo "IOCADMIN set in $EPICS_IOCS/$IOC/iocBoot/ioc$IOC/envPaths"
-			((errors--))
-			;;
-		1)
-			echo "[WARNING] IOCADMIN not set in $EPICS_IOCS/$IOC/iocBoot/ioc$IOC/envPaths"
-			;;
-		*)
-			echo "[WARNING] $EPICS_IOCS/$IOC/iocBoot/ioc$IOC/envPaths not found"
-			((missing++))
-			;;
-	esac
+	check "epicsEnvSet(\"IOCADMIN\"" "$IOCBASE/$IOC/iocBoot/ioc$IOC/envPaths" true
 
 	# Checking for iocAdminSoft.db added to DB in $EPICS_IOCS/$IOC/"$IOC"App/Db/Makefile
-	case `grep -Fx "DB += $""(IOCADMIN)/db/iocAdminSoft.db" "$EPICS_IOCS/$IOC/"$IOC"App/Db/Makefile" &> /dev/null; echo $?` in
-		0)
-			echo "iocAdminSoft added in $EPICS_IOCS/$IOC/"$IOC"App/Db/Makefile"
-			((errors--))
-			;;
-		1)
-			echo "[WARNING] iocAdminSoft not added in $EPICS_IOCS/$IOC/"$IOC"App/Db/Makefile"
-			;;
-		*)
-			echo "[WARNING] $EPICS_IOCS/$IOC/"$IOC"App/Db/Makefile not found"
-			((missing++))
-			;;
-	esac
+	check "iocAdminSoft.db" "$IOCBASE/$IOC/"$IOC"App/Db/Makefile" true
 
 	# Checking for iocAdminSoft.db in $EPICS_IOCS/$IOC/db
-	if [ -e "$EPICS_IOCS/$IOC/db/iocAdminSoft.db" ]; then
-		echo "$EPICS_IOCS/$IOC/db/iocAdminSoft.db exists"
+	if [ -e "$IOCBASE/$IOC/db/iocAdminSoft.db" ]; then
+		echo "$IOCBASE/$IOC/db/iocAdminSoft.db exists"
 		((errors--))
 	else
-		echo "[WARNING] $EPICS_IOCS/$IOC/db/iocAdminSoft.db does not exist"
+		echo "[WARNING] $IOCBASE/$IOC/db/iocAdminSoft.db does not exist"
 		((missing++))
 	fi
 	
 	# Checking for iocAdminSoft.db loaded in $EPICS_IOCS/$IOC/iocBoot/ioc$IOC/st.cmd
-	case `grep -Fx "dbLoadRecords(\"db/iocAdminSoft.db\", \"IOC=istats\")" "$EPICS_IOCS/$IOC/iocBoot/ioc$IOC/st.cmd" &> /dev/null; echo $?` in
-		0)
-			echo "iocAdminSoft loaded in $EPICS_IOCS/$IOC/iocBoot/ioc$IOC/st.cmd"
-			((errors--))
-			;;
-		1)
-			echo "[WARNING] iocAdminSoft not loaded in $EPICS_IOCS/$IOC/iocBoot/ioc$IOC/st.cmd"
-			;;
-		*)
-			echo "[WARNING] $EPICS_IOCS/$IOC/iocBoot/ioc$IOC/st.cmd not found"
-			((missing++))
-			;;
-	esac
+	check "iocAdminSoft.db" "$IOCBASE/$IOC/iocBoot/ioc$IOC/st.cmd" true
 
 	# Checking for iocAdmin.DBD added to "$IOC"_DBD in $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile
-	case `grep -Fx ""$IOC"_DBD += iocAdmin.dbd" "$EPICS_IOCS/$IOC/"$IOC"App/src/Makefile" &> /dev/null; echo $?` in
-		0)
-			echo "iocAdminSoft added in $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile"
-			((errors--))
-			;;
-		1)
-			echo "[WARNING] iocAdminSoft not added in $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile"
-			;;
-		*)
-			echo "[WARNING] $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile not found"
-			((missing++))
-			;;
-	esac
+	check "iocAdmin.dbd" "$IOCBASE/$IOC/"$IOC"App/src/Makefile" true
 	
 	# Checking for devIocStats added to "$IOC"_LIBS in $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile
-	case `grep -Fx ""$IOC"_LIBS += devIocStats" "$EPICS_IOCS/$IOC/"$IOC"App/src/Makefile" &> /dev/null; echo $?` in
-		0)
-			echo "devIocStats added in $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile"
-			((errors--))
-			;;
-		1)
-			echo "[WARNING] devIocStats not added in $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile"
-			;;
-		*)
-			echo "[WARNING] $EPICS_IOCS/$IOC/"$IOC"App/src/Makefile not found"
-			;;
-	esac
+	check "devIocStats" "$IOCBASE/$IOC/"$IOC"App/src/Makefile" false
 	
-	if [ $missing -eq 0 ]; then
+	if [ $errors -eq 0 ]; then
 		echo "This IOC meets all devIocStats requirements"
 	elif [ $errors -lt 7 ]; then
 		echo "This IOC is missing $errors devIocStats requirements"
@@ -128,5 +72,5 @@ if [ -d "$EPICS_IOCS/$IOC" ]; then
 	fi
 
 else	
-	echo "$EPICS_IOCS/$IOC does not exist"
+	echo "$IOCBASE/$IOC does not exist"
 fi
